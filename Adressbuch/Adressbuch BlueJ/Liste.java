1468ff6aea9a82eeab2402ee1db77ee28be92443
SOhrendorf <74784123+SOhrendorf@users.noreply.github.com>
2021-11-16 16:04:11 +0100

    add Funktions
    - toFirst
    - next
    -getContent
    - has Access
    - sucheName

--- Adressbuch/Adressbuch BlueJ/Liste.java
+++ Adressbuch/Adressbuch BlueJ/Liste.java
@@ -1,12 +1,42 @@
- class Liste{
+class Liste{
     Element anker;
-
-    void setup() {
-    }
+    Element aktuell;
 
     void add(Person p) {
         Element e = new Element(p);
         e.next = anker;
         anker = e;
+    }
+
+    void toFirst(){
+        aktuell = anker;  
+    }
+
+    void next(){
+        if(hasAccess()){
+            aktuell = aktuell.next;
+        }
+    }
+
+    Person getContent(){
+        return aktuell.person; 
+    }
+
+    boolean hasAccess(){
+        if(aktuell == null){
+            return false;
+        }
+        return true;
+    }
+    
+    void suchenName(String pName){
+        toFirst();
+        while(aktuell.person.name != pName){
+            next();
+        }
+        System.out.println("Kundennummer: " + aktuell.person.kundennr);
+        System.out.println("Telefonnummer: " +aktuell.person.telefonnr);
+        System.out.println("Name: " +aktuell.person.name);
+        System.out.println("Adresse: " +aktuell.person.adresse);
     }
 }
