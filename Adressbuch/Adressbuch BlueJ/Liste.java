e8df1d7ff4618546a52d1d2acd97bbf313d49a4a
SOhrendorf <74784123+SOhrendorf@users.noreply.github.com>
2021-11-22 09:07:20 +0100

    add durchlaufen

--- Adressbuch/Adressbuch BlueJ/Liste.java
+++ Adressbuch/Adressbuch BlueJ/Liste.java
@@ -29,6 +29,18 @@ class Liste{
         return true;
     }
     
+    void alleAnzeigen(){
+        toFirst();
+        while(hasAccess()){
+            Person p = getContent();
+            System.out.print(p.name);
+            System.out.print(p.adresse);
+            System.out.print(p.kundennr);
+            System.out.print(p.telefonnr);
+            next();
+        }
+    }
+    
     void suchenName(String pName){
         toFirst();
         while(aktuell.person.name != pName){
